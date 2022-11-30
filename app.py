885e293b7ce152e14e852f94c8f41ad1c880b86c
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2022-11-30 09:20:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-from streamlit_option_menu import option_menu  # pip install streamlit-option-menu
 
 
 st.title('Uber pickups in NYC')
