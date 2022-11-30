828c1611eec1bf1145abb214b4e667d28103f5d7
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2022-11-30 09:41:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,29 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+from deta import Deta
+
+# Data to be written to Deta Base
+with st.form("form"):
+    name = st.text_input("Your name")
+    age = st.number_input("Your age")
+    submitted = st.form_submit_button("Store in database")
 
 
+# Connect to Deta Base with your Project Key
+deta = Deta(st.secrets["deta_key"])
 
-st.title('Uber pickups in NYC')
+# Create a new database "example-db"
+# If you need a new database, just use another name.
+db = deta.Base("example-db")
 
-# --- HIDE STREAMLIT STYLE ---
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
+# If the user clicked the submit button,
+# write the data from the form to the database.
+# You can store any data you want here. Just modify that dictionary below (the entries between the {}).
+if submitted:
+    db.put({"name": name, "age": age})
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
-
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
-
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+"---"
+"Here's everything stored in the database:"
+# This reads all items from the database and displays them to your app.
+# db_content is a list of dictionaries. You can do everything you want with it.
+db_content = db.fetch().items
+st.write(db_content)
